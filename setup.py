c678af1de7ed5c5f3d648b304d4570808becefa7
Yanoel Llano Boitel <yllano@merthin.com>
2022-12-05 19:44:40 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-__import__('setuptools').setup()
+__import__('setuptools').setup(
+    name='onshape_launcher',
+    version='0.1.0',    
+    description='',
+    packages=['onshape_launcher'],
+    install_requires=[],
+    classifiers=[]
+)
